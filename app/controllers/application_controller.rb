a1b23fd37142f26c3230286f8f796aab083f57cd
wley3337 <wley3337@gmail.com>
2019-10-22 15:55:38 -0400

    updated cors file to take local requests, create users#login and users#create routes and controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    before_action :authenticate
+    
+    private
+    # generates the token for front end
+    def generate_token(user)
+        key_base = Rails.application.credentials.secret_key_base
+        payload = {user_id: user.id}
+        JWT.encode(payload, key_base , 'HS256')
+    end 
+
+    def current_user
+        @current_user ||= authenticate
+    end 
+
+    # validates token and returns the current user
+    def authenticate
+        #built in method w/JWT
+        authenticate_or_request_with_http_token do |token|
+            begin
+                decoded = decode(token)
+                @current_user = User.find_by(id: decoded[0]["user_id"]) 
+            rescue JWT::DecodeError
+                render json: {authorized: false }, status: 401  
+            end
+        end 
+    end 
+
+    def decode(token)
+        key_base = Rails.application.credentials.secret_key_base
+        JWT.decode(token, key_base, true, { algorithm: 'HS256' })
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
     # user.lists.sort_by &:display_order
+    skip_before_action :authenticate, only: [:login, :create]
+
+
+    def login 
+        byebug
+    end 
+
+
+    def create
+        byebug
+    end 
+
+    private 
+
+    def login_params 
+        params.require(:user).permit(:username, :password)
+    end 
 end
