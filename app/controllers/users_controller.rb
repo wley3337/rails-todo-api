4d9cb8db43301f2f5875f299f11726758456a387
wley3337 <wley3337@gmail.com>
2019-10-25 10:46:20 -0400

    added login logic in user controller for login -previous commit did not have login, just create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,25 @@ class UsersController < ApplicationController
    
     skip_before_action :authenticate, only: [:login, :create]
 
-
-    def login 
-        byebug
-    end 
-
-
     def create
         new_user = User.new(first_name: create_user_params[:firstName], last_name: create_user_params[:lastName], username: create_user_params[:username], password: create_user_params[:password])
         if new_user.save
-            byebug
             render json: { success: true, user: new_user.serialize, token: generate_token(new_user) }
         else
             render json: { success: false, errors: new_user.errors.full_messages}
         end
     end 
+
+    def login 
+        user = User.find_by(username: login_params[:username])
+        if user && user.authenticate(login_params[:password])
+            render json: { success: true, user: user.serialize, token: generate_token(user) }
+        else
+            render json: { success: false, errors: ["Wrong Username or Password"]}
+        end
+    end 
+
+
 
     private 
 
