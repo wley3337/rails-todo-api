da76cf2277bd5b85251c22e0255f8047cb21a74e
wley3337 <wley3337@gmail.com>
2019-10-31 11:36:09 -0400

    added todo creation route and action in controller

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,2 +1,23 @@
 class ToDosController < ApplicationController
+
+    def create 
+        new_to_do = ToDo.new(
+            title: create_params[:title],
+            list_id: create_params[:listId], 
+            description: create_params[:description], 
+            due: (create_params[:due]).to_datetime
+            )
+        if new_to_do.save 
+            render json: { success: true , toDo: new_to_do.serialize }
+        else 
+            render json: { success: false, messages: new_to_do.erorrs.full_messages }
+        end
+    end 
+
+    private 
+
+    def create_params 
+        # description and due are optional on front
+        params.require(:to_do).permit(:listId, :title, :description, :due)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post "/create-user", to: "users#create"
   get "/user", to: "users#show"
   resources :lists, only: [:create]
+  resources :to_dos, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
